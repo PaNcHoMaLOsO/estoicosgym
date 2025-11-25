0e0d799abbcef47d7d5abbd01b2f2a01cd497306
PaNcHoMaLOsO <mrjuanhernandez1932@gmail.com>
2025-11-25 16:49:27 -0300

    fix: Corregir ruta del logo EstoicosGym
    - Cambié ruta a vendor/adminlte/dist/img/Logo_Estoicos_Gym.svg
    - Logo ubicado en la carpeta correcta

--- config/adminlte.php
+++ config/adminlte.php
@@ -64,7 +64,7 @@ return [
     */
 
     'logo' => '<b>Estoicos</b>Gym',
-    'logo_img' => '/img/Logo_Estoicos_Gym.svg',
+    'logo_img' => 'vendor/adminlte/dist/img/Logo_Estoicos_Gym.svg',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
