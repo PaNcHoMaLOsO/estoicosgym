60c0bc9b2e48b1cce6d114e88898f9868425c387
PaNcHoMaLOsO <mrjuanhernandez1932@gmail.com>
2025-11-25 14:51:46 -0300

    feat: Agregar AdminLTE v3 vía npm con Vite
    - Instalado admin-lte v3.2.0 vía npm
    - Configurado vite.config.js para compilación de assets
    - Actualizado resources/js/app.js con imports de AdminLTE
    - Incluidas dependencias: Bootstrap 5, Font Awesome, jQuery, Chart.js, DataTables
    - Archivo package.json configurado correctamente

---
+++ resources/js/app.js
@@ -0,0 +1,23 @@
+// AdminLTE 3
+import 'admin-lte/dist/css/adminlte.min.css'
+import 'admin-lte/dist/js/adminlte.min.js'
+
+// Bootstrap y dependencias
+import 'bootstrap'
+import '@popperjs/core'
+
+// Font Awesome
+import '@fortawesome/fontawesome-free/css/all.css'
+
+// jQuery (requerido por AdminLTE)
+import 'jquery'
+
+// Chart.js
+import 'chart.js'
+
+// DataTables
+import 'datatables.net'
+import 'datatables.net-bs5'
+import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css'
+
+console.log('AdminLTE 3 cargado correctamente')
